cff751195a2b0df90bfba07e7dcd7cf59cdf0a3a
Bennett Gunson <bengunson@gmail.com>
2022-10-13 14:58:24 -0600

    add cpp compatable header

---
+++ include/pico_display_driver.hpp
@@ -0,0 +1,21 @@
+#ifndef PICO_DISPLAY_DRIVER_H
+#define PICO_DISPLAY_DRIVER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "DEV_Config.h"
+#include "fonts.h"
+#include "GUI_Paint.h"
+#include "icm20948.h"
+#include "Infrared.h"
+#include "LCD_2in.h"
+
+// TODO: include other headers for LCD/OLED screens or use namespaces/combine them into a joint header
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
